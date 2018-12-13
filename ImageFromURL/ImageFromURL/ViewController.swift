4b9d49f6a09da378ce67c950af31233ca658f8da
surabhigupta23 <36860186+surabhigupta23@users.noreply.github.com>
2018-12-13 17:03:05 +0530

    Fetch image from URL full code

---
+++ ImageFromURL/ImageFromURL/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ImageFromURL
+//
+//  Created by Surabhi Gupta on 12/7/18.
+//  Copyright © 2018 Surabhi Gupta. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageIcon: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //The image to download
+        let remoteURLImage = URL(string: "https://solarianprogrammer.com/images/2013/02/28/mandelbrot_piece_Z2.png")!
+        
+        //Use alamofire to download the image
+        //        DispatchQueue.main.asyncAfter(deadline: .now() + 5){
+        print("Enter the delay\n ")
+        Alamofire.request(remoteURLImage).responseData {
+            (response) in
+            if response.error == nil
+            {
+                print("result: ",response.result)
+                //Show the downloaded image
+                if let data = response.data {
+                    
+                    self.imageIcon.image = UIImage (data: data)
+                }
+                
+            }
+        }
+    }
+}
+
+
